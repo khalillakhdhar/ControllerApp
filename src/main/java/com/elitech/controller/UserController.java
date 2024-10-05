183b06511efe6598234f1cd04c76ef9d7ff98a76
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-10-05 12:14:31 +0100

    c

--- src/main/java/com/elitech/controller/UserController.java
+++ src/main/java/com/elitech/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.elitech.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.elitech.model.User;
 
 @RestController
 public class UserController {
@@ -9,5 +13,16 @@ public class UserController {
 	public String sayHello()
 	{
 		return "<h2>Welcome User</h2>";
+	}
+	@PostMapping("user")
+	public String getOneUser(@RequestBody User user)
+	{
+		String remarque="";
+		
+		if(user.getAge()>=18)
+			remarque="majeur";
+		else
+			remarque="mineur";
+		return user.toString()+" est "+remarque;
 	}
 }
--- src/main/java/com/elitech/model/User.java
+++ src/main/java/com/elitech/model/User.java
@@ -1,5 +1,13 @@
 package com.elitech.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+	private String nom;
+	private String prenom;
+	private int age;
 
 }
